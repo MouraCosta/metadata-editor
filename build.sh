11bc25a7665193516a2d0251441811c160d53055
moura <m0ur4@protonmail.com>
2021-09-07 20:09:40 -0300

    update: jar file name changed to "app.jar"

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 mkdir bin/;
 
-javac -d bin/ src/com/moura/Application.java;
-javac -d bin/ src/com/moura/MetadataEditor.java;
-javac -d bin/ -cp bin/ src/MetadataEditor.java;
+javac -d bin/ --release 11 src/com/moura/Application.java;
+javac -d bin/ --release 11 src/com/moura/MetadataEditor.java;
+javac -d bin/ -cp bin/ --release 11 src/MetadataEditor.java;
 
 # Build Jar process
 cd bin/;
-jar -cmf ../MANIFEST.MF ../metadataEditorApp.jar *.class com/moura/*.class;
+jar -cmf ../MANIFEST.MF ../app.jar *.class com/moura/*.class;
 cd ..;
 rm -r bin/;
 echo "\033[;32mApp build successufully";
