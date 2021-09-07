833c9c5d38051402e667afae2b112c46b15b7f78
moura <m0ur4@protonmail.com>
2021-09-07 20:19:10 -0300

    update: Application class was documented.

--- src/com/moura/Application.java
+++ src/com/moura/Application.java
@@ -2,10 +2,20 @@ package com.moura;
 
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
+import java.util.logging.Logger;
 import javax.swing.JFrame;
 
+/**
+ * Application class
+ * @author de Moura
+ */
 public class Application extends JFrame {
 
+	private GridBagConstraints c = new GridBagConstraints();
+
+	/**
+	 * Default class constructor.
+	 */
 	public Application() {
 		super("Metadata Editor");
 		setSize(600, 400);
@@ -13,6 +23,10 @@ public class Application extends JFrame {
 		setLayout(new GridBagLayout());
 	}
 
+	/**
+	 * It starts the application. In fact, this method just makes the window
+	 * visible, everything is already setup.
+	 */
 	public void start() {
 		setVisible(true);
 	}
