464be101978b9f7178860152d32a487c506917f8
moura <m0ur4@protonmail.com>
2022-05-28 23:52:01 -0300

    add: Added getNeededMetadata() method.

--- src/com/moura/MetadataEditor.java
+++ src/com/moura/MetadataEditor.java
@@ -2,6 +2,7 @@ package com.moura;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,19 +11,26 @@ import java.util.logging.Logger;
 
 /**
  * Model responsible for creating exiftool processes to change or read metadata.
+ * 
  * @author de Moura
  */
 public class MetadataEditor {
+
+	private final List<String> commonKeys = Arrays.asList(
+			"ExifTool Version Number", "File Inode Change Date/Time", "File Type",
+			"File Modification Date/Time", "MIME Type", "File Size", "File Permissions", "Directory",
+			"File Name", "File Access Date/Time");
 
 	private Logger logger = Logger.getLogger(this.getClass().getName());
 	private Map<File, Map<String, String>> memoisationTable = new HashMap<>();
 
 	/**
-	* Retrieve the metadata from a given file.
-	* @return A map containing the metadata of the file. The result is null if
-	* the file doesn't exist.
-	*/
-	//FIXME: Exception is too general.
+	 * Retrieve the metadata from a given file.
+	 * 
+	 * @return A map containing the metadata of the file. The result is null if
+	 *         the file doesn't exist.
+	 */
+	// FIXME: Exception is too general.
 	public Map<String, String> getMetadata(File file) throws Exception {
 		if (file.exists()) {
 			// Start the process of getting metadata.
@@ -34,7 +42,7 @@ public class MetadataEditor {
 				String output = getOutput(file);
 				String[] fieldsAndValues = output.split("\n");
 				metadata = new HashMap<>();
-				for (String fieldAndValue: fieldsAndValues) {
+				for (String fieldAndValue : fieldsAndValues) {
 					String[] current = fieldAndValue.split(":");
 					current[0] = current[0].strip();
 					current[1] = current[1].strip();
@@ -49,29 +57,39 @@ public class MetadataEditor {
 		}
 	}
 
+	public Map<String, String> getNeededMetadata(File file) throws Exception {
+		Map<String, String> fileMetadata = getMetadata(file);
+		for (String commonKey : commonKeys) {
+			fileMetadata.remove(commonKey);
+		}
+
+		return fileMetadata;
+	}
+
 	/**
 	 * Sets a new metadata to the file.
+	 * 
 	 * @param newMetadata A map containing all the metadata fields that needs to
-	 * be changed.
+	 *                    be changed.
 	 * @return True when the metadata
 	 */
-	//FIXME: Exception is too general.
+	// FIXME: Exception is too general.
 	public boolean setMetadata(File file, Map<String, String> newMetadata) throws Exception {
 		// TODO: Test this code and reinforce it, too much atomic
-		if (! file.exists()) {
+		if (!file.exists()) {
 			return false;
 		}
 		List<String> command = new ArrayList<>();
 		command.add("exiftool");
 		Set<String> keys = newMetadata.keySet();
-		for (String key: keys) {
+		for (String key : keys) {
 			command.add(createTag(key.replaceAll(" ", ""), newMetadata.get(key)));
 		}
 		command.add(file.getAbsolutePath());
 		logger.info(command.toString());
 		try {
 			new ProcessBuilder(command).start();
-		} catch(Exception err) {
+		} catch (Exception err) {
 			err.printStackTrace();
 		}
 		return true;
