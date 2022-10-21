38ab664798f91e775281786a5bb4d2a44a25a9be
moura <m0ur4@protonmail.com>
2022-10-20 21:21:25 -0300

    update: Use a FXML file instead of hard coding the whole UI.

--- src/main/java/com/moura/app/App.java
+++ src/main/java/com/moura/app/App.java
@@ -1,130 +1,48 @@
 package com.moura.app;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
 import java.io.File;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.UIManager;
 
 import com.moura.MetadataEditor;
-import com.moura.actions.AddNewFieldAction;
-import com.moura.actions.DeleteFieldAction;
-import com.moura.actions.SaveChangesAction;
-import com.moura.actions.SelectFileAction;
-import com.moura.components.FilenameIconLabel;
 import com.moura.components.MetadataFields;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
 
 /**
  * Application class
  * 
  * @author de Moura
  */
-public class App extends JFrame {
-
-	public FilenameIconLabel filenameIconLabel = new FilenameIconLabel(this);
-	public MetadataFields metadataFields = new MetadataFields(this);
-	public JButton saveButton = new JButton("Save"), selectFileButton = new JButton("Select File"),
-			addFieldButton = new JButton("+"), deleteFieldButton = new JButton("-");
-
+public class App extends Application {
+	public MetadataFields metadataFields = new MetadataFields();
 	public boolean fileSelected;
 	public File onChange;
 	private MetadataEditor metadataEditor = new MetadataEditor();
 
-	/**
-	 * Default class constructor.
-	 */
-	public App() {
-		super("Metadata Editor");
-		setIconImage(new ImageIcon("data/icon.png").getImage());
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setLayout(new GridBagLayout());
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		Parent root = FXMLLoader.load(getClass().getResource("ui.fxml"));
+		Scene scene = new Scene(root, 600, 400);
+	
+		primaryStage.setScene(scene);
 
-		setupComponents();
-		setSize(600, 400);
-		setMinimumSize(getSize());
-	}
+		primaryStage.setTitle("Metadata Editor");
+		primaryStage.setMinWidth(640);
+		primaryStage.setMinHeight(480);
 
-	/**
-	 * It starts the application. In fact, this method just makes the window
-	 * visible, everything is already setup.
-	 */
-	public void start() {
-		setVisible(true);
+		primaryStage.show();
 	}
 
 	public static void main(String[] args) {
-		// Set App look and feel to the system look and feel
-		try {
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		} catch (Exception err) {
-			err.printStackTrace();
-		}
-
-		App app = new App();
-		app.start();
-	}
-
-	private void setupButtons() {
-		// Add functions to the buttons.
-		selectFileButton.addActionListener(new SelectFileAction(this, metadataEditor));
-		saveButton.addActionListener(new SaveChangesAction(this, metadataEditor));
-		addFieldButton.addActionListener(new AddNewFieldAction(this));
-		deleteFieldButton.addActionListener(new DeleteFieldAction(this));
-
-		JPanel buttonsPanel = new JPanel(new GridBagLayout());
-
-		// Setups the addField button in the application view.
-		GridBagConstraints addFieldButtonConstraints = new GridBagConstraints();
-		addFieldButtonConstraints.gridx = 1;
-		addFieldButtonConstraints.gridy = 1;
-		addFieldButtonConstraints.weightx = 1;
-		addFieldButtonConstraints.anchor = GridBagConstraints.WEST;
-		add(addFieldButton, addFieldButtonConstraints);
-
-		// Setups the delete field button in the application view.
-		GridBagConstraints deleteButtonConstraints = new GridBagConstraints();
-		deleteButtonConstraints.gridx = 1;
-		deleteButtonConstraints.gridy = 1;
-		deleteButtonConstraints.weightx = 1;
-		deleteButtonConstraints.anchor = GridBagConstraints.WEST;
-		deleteButtonConstraints.insets = new Insets(0, 45, 0, 0);
-		add(deleteFieldButton, deleteButtonConstraints);
-
-		// Setups the selectFile button in the application view.
-		GridBagConstraints selectFileButtonConstraints = new GridBagConstraints();
-		selectFileButtonConstraints.weightx = 1;
-		selectFileButtonConstraints.weighty = 1;
-		selectFileButtonConstraints.anchor = GridBagConstraints.NORTHWEST;
-		selectFileButtonConstraints.gridx = 0;
-		selectFileButtonConstraints.gridy = 0;
-		buttonsPanel.add(selectFileButton, selectFileButtonConstraints);
-
-		// Setups the save button in the application view.
-		GridBagConstraints saveButtonConstraints = new GridBagConstraints();
-		saveButtonConstraints.weightx = 1;
-		saveButtonConstraints.weighty = 1;
-		saveButtonConstraints.gridx = 1;
-		saveButtonConstraints.gridy = 0;
-		saveButtonConstraints.anchor = GridBagConstraints.NORTHEAST;
-		buttonsPanel.add(saveButton, saveButtonConstraints);
-
-		// Setups the panel that hold the save and selectFile buttons.
-		GridBagConstraints panelConstraints = new GridBagConstraints();
-		panelConstraints.gridx = 1;
-		panelConstraints.gridy = 2;
-		panelConstraints.weightx = 1;
-		panelConstraints.anchor = GridBagConstraints.WEST;
-		panelConstraints.insets = new Insets(30, 0, 30, 0);
-		add(buttonsPanel, panelConstraints);
-	}
-
-	private void setupComponents() {
-		filenameIconLabel.add();
-		metadataFields.add();
-		setupButtons();
+		launch(args);
 	}
 }
