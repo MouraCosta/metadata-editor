3b80ffd61b749e1f666ef5c68066bdeb64365fbd
moura <m0ur4@protonmail.com>
2022-11-12 11:59:27 -0300

    Add: Added an icon to the app.

--- src/main/java/com/moura/metadataeditor/MetadataEditorApp.java
+++ src/main/java/com/moura/metadataeditor/MetadataEditorApp.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -17,7 +18,8 @@ public class MetadataEditorApp extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		Parent root = FXMLLoader.load(this.getClass().getResource("/views/main.fxml"));
 		Scene scene = new Scene(root, 600, 400);
-
+		primaryStage.getIcons().add(new Image(
+			this.getClass().getResourceAsStream("/images/icon.png")));
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Metadata Editor");
 		primaryStage.setMinWidth(640);
