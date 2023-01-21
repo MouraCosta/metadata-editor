e0a6ad2e7d2e878deaee8ce3ebebb3ab28683741
moura <m0ur4@protonmail.com>
2023-01-20 23:11:15 -0300

    update: Make project non-modular and implement deb packager script

---
+++ src/main/java/com/moura/metadataeditor/Launcher.java
@@ -0,0 +1,10 @@
+package com.moura.metadataeditor;
+
+import com.moura.metadataeditor.app.*;
+
+public class Launcher {
+    
+    public static void main(String[] args) {
+        MetadataEditorApp.main(args);
+    }
+}
--- src/main/java/com/moura/metadataeditor/app/MetadataEditorApp.java
+++ src/main/java/com/moura/metadataeditor/app/MetadataEditorApp.java
@@ -1,4 +1,4 @@
-package com.moura.metadataeditor;
+package com.moura.metadataeditor.app;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
